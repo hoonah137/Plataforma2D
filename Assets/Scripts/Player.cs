0bf3aba791d35f277260e51cd50eb0e532dbe5de
hoonah137 <121156401+hoonah137@users.noreply.github.com>
2023-09-20 09:46:46 +0200

    movimiento por velocidad

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,26 +6,47 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float _vel = 7f;
     float _playerInput;
-    float _vertical;
+    //float _vertical;
+
+    //componentes
+    Rigidbody2D _rBody2D;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       Move();
+       
 
     }
 
-    void Move()
+    void FixedUpdate()
     {
-         _playerInput = Input.GetAxis("Horizontal");
-        _vertical = Input.GetAxis("Vertical");
-        transform.Translate(new Vector2(_playerInput, _vertical) * _vel * Time.deltaTime );        
+        Move();
+    }
+
+      void Move()
+    {
+        _playerInput = Input.GetAxis("Horizontal");
+
+        _rBody2D.velocity = new Vector2(_playerInput * _vel, 0);
+        
+          
+
+        /*
+         Movimiento sin fisicas
+        
+            _playerInput = Input.GetAxis("Horizontal");
+            _vertical = Input.GetAxis("Vertical");
+            transform.Translate(new Vector2(_playerInput, _vertical) * _vel * Time.deltaTime ); 
+        
+        Movimiento add force (es ago erratico)
+            _rBody2D.AddForce(new Vector2(_playerInput * _vel, 0), ForceMode2D.Impulse);  
+        */
     }
 
     
